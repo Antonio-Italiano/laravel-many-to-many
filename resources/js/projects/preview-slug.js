27d151dd71c6950bb0cca351900acda47a37e926
Antonio-Italiano <antoitalia221@gmail.com>
2023-03-14 18:35:30 +0100

    revised -slug-preview

--- resources/js/projects/preview-slug.js
+++ resources/js/projects/preview-slug.js
@@ -3,6 +3,6 @@ const slugInput = document.getElementById('slug');
 const titleInput = document.getElementById('title');
 
 //metto un event listner sul title
-titleInput.addEventListner('blur', () => {
+titleInput.addEventListener('keypress', () => {
   slugInput.value = titleInput.value.toLowerCase().split(' ').join('-');
 });

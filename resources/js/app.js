680b62b0b82b1556f91f9908763b8f8e52f9a09e
Antonio-Italiano <antoitalia221@gmail.com>
2023-03-14 18:36:49 +0100

    imported file °preview-img °delete-confirm °preview-slug

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,10 @@
 import './bootstrap';
 import '~resources/scss/app.scss';
+// aggiunti
+import './projects/delete-confirm';
+import './projects/preview-img';
+import './projects/preview-slug';
+
 import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
